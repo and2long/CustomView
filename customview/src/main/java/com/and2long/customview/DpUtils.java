81ff9aaec6e5d50e640b0d3526a640fbcea11024
and2long <and2long@gmail.com>
2018-03-18 01:25:13 +0800

    gradle文件修改

--- customview/src/main/java/com/and2long/customview/DpUtils.java
+++ customview/src/main/java/com/and2long/customview/DpUtils.java
@@ -9,7 +9,9 @@ import android.content.Context;
 public class DpUtils {
 
     /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     * @param context 上下文
+     * @param dpValue value
+     * @return value
      */
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
@@ -18,7 +20,9 @@ public class DpUtils {
 
 
     /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     * @param context 上下文
+     * @param pxValue value
+     * @return value
      */
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
